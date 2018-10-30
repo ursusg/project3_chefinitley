7aa9b8604cec8bcec41744d494a8e2294d738426
Phenom Phons <alphonstate@gmail.com>
2018-10-30 18:25:34 -0500

    home

---
+++ client/src/components/Input/Input.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Row, Input, Col } from "react-materialize";
+
+const search = () => (
+  <Row className="container">
+  <Col s={6}>
+    <Input
+      className="#ffd54f "
+      s={6}
+      label="City"
+      validate
+      defaultValue="Chicago"
+    />
+    </Col>
+    <Col s={6}>
+    <Input
+      className="#ffd54f "
+      s={6}
+      label="Cuisine"
+      validate
+      defaultValue="Pizza"
+    />
+    </Col>
+  </Row>
+);
+export default search;
---
+++ client/src/pages/Homie/Homie.js
@@ -0,0 +1,25 @@
+import React  from 'react';
+ import Carousel from "../../components/Carousel";
+import Search from "../../components/Input";
+import Footer from "../../components/Footer";
+import Icons from "../../components/Icons";
+import Card from "../../components/Card"
+
+
+
+
+
+
+const Home = () => (
+  
+      <div>
+        <Carousel/>
+        <Search />
+        <Icons />
+        <Card />
+        <Footer />
+
+        </div>
+    );
+  
+  export default Home;
