c595ccbfcb8b6c76bb47b850579676391c6a17f6
Azalea <Rodriguez32.ilda0122@gmail.com>
2018-10-31 12:08:52 -0500

    routes, modal

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,14 +1,25 @@
 import React from "react";
-import { Navbar, NavItem } from "react-materialize";
+import { Navbar, NavItem, Button, Modal, Row, Input, Col } from "react-materialize";
 import ImportedImage from "../../images/Logo(transparent).PNG";
 import "./Nav.css";
 
 var Img = <img alt="ime" class="logo" src={ImportedImage} />;
 
 export default () => (
-  <Navbar  brand={Img} className="grey darken-2 appbar" left>
-    <NavItem onClick={() => console.log("test click")}>Login</NavItem>
-    <NavItem href="/becomechef">Become a Chef</NavItem>
-    <NavItem href="/becomechef">Create Account</NavItem>
+  <Navbar brand={Img} className="grey darken-2 appbar" left>
+    
+    <NavItem href="/Signup">Chef Account</NavItem>
+    <NavItem href="/SignUpCustomer">Customer Account</NavItem>
+    <Modal className="center" header="Log In with your Chefinately Account" trigger={<Button  className="brown">LogIn</Button>}>
+      <Row>
+        <Col className="center" s={12}>
+        <Input  type="email" label="Email" s={6} />
+        </Col>
+        <Col className="center" s={12}>
+        <Input  type="password" label="password" s={6} />
+        </Col>
+        <Button waves='light'>Sign In</Button>
+      </Row>
+    </Modal>
   </Navbar>
 );
--- client/src/pages/About/AboutUs.js
+++ client/src/pages/About/AboutUs.js
@@ -4,13 +4,12 @@ import { Col, Card } from "react-materialize";
 const AboutUs = () => {
   return (
     <div className="container">
-      {" "}
       <Col m={6} s={12}>
         <Card
           className="blue-grey darken-1"
           textClassName="white-text"
           title="About Us"
-          actions={[<a href="#">This is a link</a>]}
+          actions={[<a href="/">Contact Us</a>]}
         >
           Chefinitly provides independent chefs a service to connect with customers who are looking for home-cooked meals. As well as providing chefs with a platform to cater to any number of individuals. 
          A chef can decide whether they’re making a specialty dinner of the day or spending time catering for an event. Customers will be able to specify their need for a specific cuisine and request a specific chef.
