29b7ec235c3cd07cd837b9705c581bc02ee337c8
Ursus <ugurungsus@gmail.com>
2018-11-03 10:21:55 -0500

    Fixed signupchef.js conflicts and added some state changes to signupchef.js

--- client/src/pages/Signup/signupchef.js
+++ client/src/pages/Signup/signupchef.js
@@ -19,6 +19,33 @@ class SignUpChef extends Component {
   formSubmit = event => {
     event.preventDefault();
     console.log("Submit button works!");
+  
+    event.preventDefault()
+
+    var chefData = {
+      chefName: this.state.chefName,
+      email: this.state.email,
+      password: this.state.password,
+      foods: this.state.foods,
+      description: this.state.description,
+      availability: this.state.availability,
+      city: this.state.city,
+      range: this.state.range
+    }
+
+    console.log(this.state)
+    API.saveChef({
+      chefData
+    })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
   };
 
   render() {
@@ -42,7 +69,8 @@ class SignUpChef extends Component {
             s={12}
             multiple
             placeholder="Upload one or more files"
-          />
+            >
+            </Input>
         </Row>
 
         {/* <Row>
@@ -58,17 +86,30 @@ class SignUpChef extends Component {
         </Row> */}
 
         <Row>
-          <Input type="textarea" label="Availability" s={6} />
+          <Input type="textarea" label="Availability" s={6}/>
         </Row>
 
         <Row>
-          <Input s={6} type="select" label="City" defaultValue="2">
+          <Input s={6} type="select" label="City" defaultValue="2"/>
+          <Input onChange={this.handleInputChange} s={6} label="Chef Name" name="chefName" />
+          <Input onChange={this.handleInputChange} type="email" label="Email" s={12} name="email" />
+          <Input onChange={this.handleInputChange} type="password" label="password" s={12} name="password" />
+        </Row>
+
+        <Row>
+          <Input type="textarea" label="Menu" name="foods"/>
+          <Input onChange={this.handleInputChange} type="textarea" label="Description" name="description" />
+          <Input onChange={this.handleInputChange} type="textarea" label="Availability" name="availability" />
+        </Row>
+
+        <Row>
+          <Input onChange={this.handleInputChange} s={12} type="select" label="City" defaultValue="2" name="city">
             <option value="1">Chicago</option>
             <option value="2">Evanston</option>
             <option value="3">Des Plaines</option>
           </Input>
 
-          <Input s={6} type="select" label="Range" defaultValue="2">
+          <Input onChange={this.handleInputChange} s={12} type="select" label="Range" defaultValue="2" name="range">
             <option value="1">0-5 Miles</option>
             <option value="2">0-10 Miles</option>
             <option value="3">0-15 Miles</option>
@@ -76,8 +117,9 @@ class SignUpChef extends Component {
         </Row>
 
         <Row>
-          <Button className="orange" onclick={this.formSubmit}>Submit</Button>
+          <Button className="orange" onClick={this.formSubmit}>Submit</Button>
         </Row>
+        
       </div>
     );
   }
