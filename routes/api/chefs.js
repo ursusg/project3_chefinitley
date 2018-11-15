199656a6b1bd1640f9fd581f53ec200df82de999
mandoh <mandomartinez2017@gmail.com>
2018-11-14 20:28:44 -0600

    fucking work

--- routes/api/chefs.js
+++ routes/api/chefs.js
@@ -1,45 +1,12 @@
 const router = require("express").Router();
 // const { check, validationResult } = require('express-validator');
 const chefController = require("../../controllers/chefController");
-const Chef = require("../../models/chefs")
 
-// Matches with "/api/chef/signup"
-router.route("/signup")
+// Matches with "/api/chef"
+router.route("/")
   .get(chefController.findAll)
   .post(chefController.create);
 
-// Matches with "/api/chef/login"
-router.route("/login")
-  .post((req, res, next) => {
-    const { body } = req;
-    let { password, email} = body;
-
-    email = email.toLowerCase();
-
-    chefController.find({
-      email: email
-    }, (error, chefs) => {
-      if (error) {
-        return res.send({
-          success: false,
-          message: "Something went wrong."
-        })
-      }
-      else if (chefs.length != 1) {
-        return res.send({
-          success: false,
-          message: "Invalid email"
-        })
-      }
-      
-      const user = chefs[0]
-      
-    })
-
-    if (!password) {
-
-    }
-  })
 
 // Matches with "/api/chef/:id"
 router
@@ -47,6 +14,11 @@ router
   .get(chefController.findById)
   .put(chefController.update)
   .delete(chefController.remove);
+
+// // Routes with "/signup"
+// router
+//   .route("/signup")
+//   .post(chefController.create);
 
 // router
 //   .route("/api/chefs")
